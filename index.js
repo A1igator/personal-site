c4aa1991abb9c07e4280e951275c091ae85962a7
A1igator <aliabdoli1970@gmail.com>
2024-03-25 21:21:41 -0400

    try to add new pages

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+w3.includeHTML();
+
 const isDark = () => document.documentElement.classList.contains("dark-theme");
 const changeTheme = () => {
   document.documentElement.classList.toggle("dark-theme");
@@ -10,7 +12,6 @@ const changeContentBasedOnTheme = () => {
   const aliImg = document.getElementById("aliImg");
   const header = document.getElementById("name");
   const themeIcon = document.getElementById("themeIcon");
-  const quote = document.getElementById("quote");
 
   if (isDarkMode) {
     aliImg.classList.add("hide-img");
@@ -18,16 +19,12 @@ const changeContentBasedOnTheme = () => {
     header.innerText = "Gator";
     document.title = "Gator";
     themeIcon.innerHTML = feather.icons["sun"].toSvg();
-    quote.innerHTML =
-      "\"I write what's in my heart. Don't give a f@#k who is f@#king with it\" - J Cole";
   } else {
     aliImg.classList.remove("hide-img");
     gatorImg.classList.add("hide-img");
     header.innerText = "Ali Abdoli";
     document.title = "Ali Abdoli";
     themeIcon.innerHTML = feather.icons["moon"].toSvg();
-    quote.innerHTML =
-      '"Life is a balance. You lose your grip you can fall into an abyss." - J Cole';
   }
 };
 
